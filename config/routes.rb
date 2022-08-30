2f2905b36eb059ad1ea56a13971292a63e0409c6
Lucine Jaff <lucinejaff@gmail.com>
2022-08-30 17:41:50 +0200

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  
-    resources :chatrooms, only: :show do
-    resources :messages, only: :create
 
-  get "profile", to: "pages#profile", as: :profile
+    resources :chatrooms, only: :show do
+      resources :messages, only: :create
+    end
+
+    get "profile", to: "pages#profile", as: :profile
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index
 
     # get 'profile', to: 'users#profile'
     # get 'interests', to: 'interests#profile'
-    
+
     resources :plants do
        resources :interests, only: [:new, :create]
    #   resources :reviews, only: [:new]
@@ -20,5 +21,4 @@ Rails.application.routes.draw do
     resources :interests, only: [:index, :show, :destroy]
     # resources :reviews
 
-  end
 end
