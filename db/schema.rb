9d77a54e6c398e821a0f4841c6780eb557dff2c5
Brane Bencic <brane.bencic@gmail.com>
2022-09-01 18:02:07 +0200

    not complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_01_075921) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
-    t.bigint "user_1_id"
-    t.bigint "user_2_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_1_id"], name: "index_chatrooms_on_user_1_id"
-    t.index ["user_2_id"], name: "index_chatrooms_on_user_2_id"
+  end
+
+  create_table "interests", force: :cascade do |t|
+    t.boolean "match", default: false
+    t.bigint "plant_interest_id"
+    t.bigint "plant_exchange_id"
+    t.bigint "user_interest_id"
+    t.bigint "user_exchange_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_exchange_id"], name: "index_interests_on_plant_exchange_id"
+    t.index ["plant_interest_id"], name: "index_interests_on_plant_interest_id"
+    t.index ["user_exchange_id"], name: "index_interests_on_user_exchange_id"
+    t.index ["user_interest_id"], name: "index_interests_on_user_interest_id"
   end
 
   create_table "marks", force: :cascade do |t|
@@ -47,20 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_075921) do
     t.index ["plant_2_id"], name: "index_matches_on_plant_2_id"
     t.index ["user_1_id"], name: "index_matches_on_user_1_id"
     t.index ["user_2_id"], name: "index_matches_on_user_2_id"
-  end
-
-  create_table "interests", force: :cascade do |t|
-    t.boolean "match", default: false
-    t.bigint "plant_interest_id"
-    t.bigint "plant_exchange_id"
-    t.bigint "user_interest_id"
-    t.bigint "user_exchange_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["plant_exchange_id"], name: "index_interests_on_plant_exchange_id"
-    t.index ["plant_interest_id"], name: "index_interests_on_plant_interest_id"
-    t.index ["user_exchange_id"], name: "index_interests_on_user_exchange_id"
-    t.index ["user_interest_id"], name: "index_interests_on_user_interest_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -102,21 +96,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_075921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "interests", "plants", column: "plant_exchange_id"
   add_foreign_key "interests", "plants", column: "plant_interest_id"
   add_foreign_key "interests", "users", column: "user_exchange_id"
   add_foreign_key "interests", "users", column: "user_interest_id"
-
-  add_foreign_key "chatrooms", "users", column: "user_1_id"
-  add_foreign_key "chatrooms", "users", column: "user_2_id"
   add_foreign_key "marks", "plants"
   add_foreign_key "marks", "users"
   add_foreign_key "matches", "plants", column: "plant_1_id"
   add_foreign_key "matches", "plants", column: "plant_2_id"
   add_foreign_key "matches", "users", column: "user_1_id"
   add_foreign_key "matches", "users", column: "user_2_id"
-
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
   add_foreign_key "plants", "users"
