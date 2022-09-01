6fa97cd7d2725339b35d31f5cb7eb2e2eb802b46
Emre Ebeturk <emreebeturk@gmail.com>
2022-09-01 12:56:30 +0200

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_080058) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_075921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
+    t.bigint "user_1_id"
+    t.bigint "user_2_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_1_id"], name: "index_chatrooms_on_user_1_id"
+    t.index ["user_2_id"], name: "index_chatrooms_on_user_2_id"
   end
 
   create_table "marks", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "plant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.bigint "plant_id", null: false
     t.index ["plant_id"], name: "index_marks_on_plant_id"
     t.index ["user_id"], name: "index_marks_on_user_id"
   end
 
   create_table "matches", force: :cascade do |t|
+    t.bigint "user_1_id"
+    t.bigint "user_2_id"
+    t.bigint "plant_1_id"
+    t.bigint "plant_2_id"
+    t.boolean "exchanged", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "plant_id", null: false
-    t.bigint "user_id", null: false
-    t.index ["plant_id"], name: "index_matches_on_plant_id"
-    t.index ["user_id"], name: "index_matches_on_user_id"
+    t.index ["plant_1_id"], name: "index_matches_on_plant_1_id"
+    t.index ["plant_2_id"], name: "index_matches_on_plant_2_id"
+    t.index ["user_1_id"], name: "index_matches_on_user_1_id"
+    t.index ["user_2_id"], name: "index_matches_on_user_2_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -77,10 +86,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_080058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chatrooms", "users", column: "user_1_id"
+  add_foreign_key "chatrooms", "users", column: "user_2_id"
   add_foreign_key "marks", "plants"
   add_foreign_key "marks", "users"
-  add_foreign_key "matches", "plants"
-  add_foreign_key "matches", "users"
+  add_foreign_key "matches", "plants", column: "plant_1_id"
+  add_foreign_key "matches", "plants", column: "plant_2_id"
+  add_foreign_key "matches", "users", column: "user_1_id"
+  add_foreign_key "matches", "users", column: "user_2_id"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
   add_foreign_key "plants", "users"
