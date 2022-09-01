f3f271c84abc6a3adcb713cf9d611303bdf0947f
Emre Ebeturk <emreebeturk@gmail.com>
2022-09-01 14:37:47 +0200

    routes revised

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index
 
-    # get 'profile', to: 'users#profile'
-    # get 'interests', to: 'interests#profile'
+
     resources :users, only: [:index, :show]
 
 
