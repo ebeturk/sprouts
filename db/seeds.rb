d319ca7e64ae89e9195e86566321d5f84702b544
Brane Bencic <brane.bencic@gmail.com>
2022-09-06 16:55:19 +0200

    Seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,74 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
 
-# Mark.destroy_all
-# Plant.destroy_all
-# Chatroom.destroy_all
-# User.destroy_all
-# Message.destroy_all
 
-user = User.create!(
-  email: "logosemre@gmail.com",
-  password: 123456,
-  username: "Emre",
-  description: "Hi! This is Emre. A philosopher, musician, programmer and in love with green."
-)
+Message.destroy_all
+Chatroom.destroy_all
+Match.destroy_all
+Mark.destroy_all
+Plant.destroy_all
+User.destroy_all
 
-  mango = Plant.create!( name: "Mango", category: "Trees", lighting: "🌞🌞:sun_with_face:", watering: ":droplet::droplet:", temperature: "🌡🌡🌡", user_id: user.id, latitude: 52.4349, longitude: 13.6351)
-  sanseveria = Plant.create!( name: "Sanseveria", category: "House Plants", lighting: "🌞🌞:sun_with_face:", watering: ":droplet::droplet:", temperature: "🌡🌡", user_id: user.id, latitude: 53.0349, longitude: 14.0351)
-  monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: ":sun_with_face:", watering: ":droplet:", temperature: "🌡🌡🌡", user_id: user.id, latitude: 51.8349, longitude: 12.9051)
-  pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞:sun_with_face:", watering: ":droplet::droplet:", temperature: "🌡🌡", user_id: user.id, latitude: 52.8349, longitude: 13.1351)
+puts "Creating users and plants. And chatrooms also..."
 
-puts "Emre is born! but where is Brane 👻"
+1.times do |n|
+  user = User.create!(
+    email: "brane.bencic@gmail.com",
+    password: 123456,
+    username: "Brane",
+    description: "I am an enthusiastic programmer and a plant lover."
+  )
 
-# Create Rafi
+  mango = Plant.create!( name: "Mango", category: "Trees", lighting: "🌞🌞🌞", watering: "💧💧", temperature: "🌡🌡🌡", user_id: user.id, latitude: 52.4349, longitude: 13.6351)
+  sanseveria = Plant.create!( name: "Sanseveria", category: "House Plants", lighting: "🌞🌞🌞", watering: "💧💧", temperature: "🌡🌡", user_id: user.id, latitude: 53.0349, longitude: 14.0351)
+  monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", temperature: "🌡🌡🌡", user_id: user.id, latitude: 51.8349, longitude: 12.9051)
+  pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", temperature: "🌡🌡", user_id: user.id, latitude: 52.8349, longitude: 13.1351,)
+
+end
+puts "Brane is born!"
+
+1.times do |n|
+  user = User.create!(
+    email: "lucinejaff@gmail.com",
+    password: 123456,
+    username: "Lucine",
+    description: "I love my plants, coding, and meditation."
+  )
+
+  mango = Plant.create!( name: "Mango", category: "Trees", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.4349, longitude: 13.6351)
+  sanseveria = Plant.create!( name: "Sanseveria", category: "House Plants", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 53.0349, longitude: 14.0351)
+  monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", user_id: user.id, latitude: 51.8349, longitude: 12.9051)
+  pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.8349, longitude: 13.1351)
+
+end
+
+puts "Lucine is born!"
+
+1.times do |n|
+  user = User.create!(
+    email: "logosemre@gmail.com",
+    password: 123456,
+    username: "Emre",
+    description: "Hi! This is Emre. A philosopher, musician, programmer and in love with green."
+  )
+
+  rubber_plant = Plant.create!(name: "Rubber Plant", category: "House Plants", lighting: "🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.5200, longitude: 13.4050)
+  pilea = Plant.create!( name: "Pilea Plant", category: "House Plants", lighting: "🌞", watering: "💧💧",  user_id: user.id, latitude: 52.4905, longitude: 13.3145)
+  palm = Plant.create!( name: "Palm Tree", category: "Trees", lighting: "🌞🌞🌞", watering: "💧",  user_id: user.id, latitude: 52.4983, longitude: 13.4070)
+  red_prayer = Plant.create!( name: "Red Prayer", category: "House Plants", lighting: "🌞🌞", watering: "💧💧",  user_id: user.id, latitude: 52.5588, longitude: 13.2884)
+
+  mango = Plant.create!( name: "Mango", category: "Trees", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.4349, longitude: 13.6351)
+  sanseveria = Plant.create!( name: "Sanseveria", category: "House Plants", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 53.0349, longitude: 14.0351)
+  monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", user_id: user.id, latitude: 51.8349, longitude: 12.9051)
+  pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.8349, longitude: 13.1351)
+
+end
+
+puts "Emre is born!"
+
+1.times do |n|
   user = User.create!(
     email: "raphaellasidb@gmail.com",
     password: 123456,
@@ -44,10 +90,11 @@ puts "Emre is born! but where is Brane 👻"
   monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", user_id: user.id, latitude: 51.8349, longitude: 12.9051)
   pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.8349, longitude: 13.1351)
 
+end
 
 puts "Rafi is born!"
 
-# Create Sprouty
+1.times do |n|
   user = User.create!(
     email: "claire@queen.com",
     password: 123456,
@@ -60,11 +107,14 @@ puts "Rafi is born!"
   monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", user_id: user.id, latitude: 51.8349, longitude: 12.9051)
   pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.8349, longitude: 13.1351)
 
+end
+
 puts "Sprouty is born!"
 
-Chatroom.create!(name: "Gingko House", user_1: User.first, user_2: User.third)
-Chatroom.create!(name: "Cedar House", user_1: User.second, user_2: User.first)
-Chatroom.create!(name: "Cedar House", user_1: User.second, user_2: User.third)
+Chatroom.create!(name: "Gingko House", user_1_id: User.first.id, user_2_id: User.last.id)
+Chatroom.create!(name: "Cedar House", user_1_id: User.second.id, user_2_id: User.first.id)
+Chatroom.create!(name: "Pine House", user_1_id: User.third.id, user_2_id: User.first.id)
+Chatroom.create!(name: "Oak House", user_1_id: User.last.id, user_2_id: User.second.id)
 
 puts "Chatrooms Gingko House, Cedar House, Pine House, and Oak House are created!"
 
