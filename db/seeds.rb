425c44dd490040f162cec182c12a1f22f6572ac1
Emre Ebeturk <emreebeturk@gmail.com>
2022-09-06 17:00:52 +0200

    info window again

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ puts "Brane is born!"
   )
 
   monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", user_id: user.id, latitude: 51.8349, longitude: 12.9051)
-  pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.8349, longitude: 51.8349, longitude: 12.9051)
+  pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.8349, longitude: 12.9051)
 
 end
 
@@ -70,7 +70,7 @@ puts "Emre is born!"
   )
 
   rubber_plant = Plant.create!(name: "Rubber Plant", category: "House Plants", lighting: "🌞🌞", watering: "💧💧", user_id: user.id, latitude: 52.5200, longitude: 13.4050)
-  pothos = Plant.create!( name: "Pothos Plant", category: "Climbers", lighting: "🌞🌞🌞", watering: "💧💧", user_id: user.id, 52.5200, longitude: 13.4050)
+  nepenthes = Plant.create!( name: "Nepenthes", category: "House Plants", lighting: "🌞🌞🌞🌞", watering: "💧💧💧💧", user_id: user.id, latitude: 52.5200, longitude: 13.4050)
 
 end
 
@@ -85,7 +85,7 @@ puts "Rafi is born!"
   )
 
   palm = Plant.create!( name: "Palm Tree", category: "Trees", lighting: "🌞🌞🌞", watering: "💧",  user_id: user.id, latitude: 52.4983, longitude: 13.4070)
-  monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", user_id: user.id, 52.4983, longitude: 13.4070)
+  monstera = Plant.create!( name: "Monstera", category: "House Plants", lighting: "🌞", watering: "💧", user_id: user.id, latitude: 52.4983, longitude: 13.4070)
 
 end
 
