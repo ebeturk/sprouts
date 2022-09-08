86909d79cae22a191d8b02520a3b4182deb98d06
Emre Ebeturk <emreebeturk@gmail.com>
2022-09-08 14:32:01 +0200

    link to back to initial

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,10 +3,10 @@ class ChatroomsController < ApplicationController
     @chatrooms = Chatroom.all
   end
 
-
+  
 
   def show
-    @chatroom = Chatroom.where( )
+    @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
 end
