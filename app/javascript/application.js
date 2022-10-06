b708ad9546b6989a65b1c45f9e80c7e37374dab3
Emre Ebeturk <emreebeturk@gmail.com>
2022-10-06 12:35:44 +0200

    js module deleted before the push to heroku

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,13 +6,7 @@ import Swiper from 'swiper';
   // import Swiper styles
   import 'swiper/css';
 
-  // const swiper = new Swiper(...);
-
-// import Swiper, { Navigation, Pagination } from 'swiper';
-//   // import Swiper and modules styles
-//   import 'swiper/css';
-//   import 'swiper/css/navigation';
-//   import 'swiper/css/pagination';
+  
 
   // init Swiper:
   const swiper = new Swiper('.swiper', {
