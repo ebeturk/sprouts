8f9fda06c3a7cd9241af495b61784ba03f7af84e
Emre Ebeturk <emreebeturk@gmail.com>
2022-10-06 17:02:41 +0200

    swiper module issue resolved

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,23 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-import Swiper from 'swiper';
-  // import Swiper styles
-  import 'swiper/css';
 
-  
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
 
-  // init Swiper:
-  const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation, Pagination],
-    ...
-  });
-
-  // import Swiper from 'swiper/bundle';
-
-  // import styles bundle
-  import 'swiper/css/bundle';
-
-  // init Swiper:
-  // const swiper = new Swiper(...);
